a4d38feb689e86ead519c56f9211ae448d103edb
Kai Peterson <pete9372@umn.edu>
2019-10-23 10:30:10 -0500

    solve base goals

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+        <p>We are a really really really good looking organization</p>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Contact</h2>
+        <p>Phone #: 999-999-9999</p>
+        <p>Email: super-cool-person@gmail.com</p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
